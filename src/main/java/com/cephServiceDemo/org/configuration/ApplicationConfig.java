705da0d9474dd4373e4c048ba61a3a3431003b6a
Mithun Debnath <mithun.4.work@gmail.com>
2018-05-23 15:31:12 +0530

    Configuration changed
    Authorization credentials added

--- src/main/java/com/cephServiceDemo/org/configuration/ApplicationConfig.java
+++ src/main/java/com/cephServiceDemo/org/configuration/ApplicationConfig.java
@@ -4,9 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 import com.mongodb.MongoClient;
 
@@ -24,10 +34,16 @@ public class ApplicationConfig {
 	@Value("${spring.data.mongodb.host}")
 	private String host;
 		
+	@Value("${spring.data.mongodb.username}")
+	private String userName;
+	
+	@Value("${spring.data.mongodb.password}")
+	private String password;
+	
     @Bean
     public MongoDbFactory mongoDbFactory() throws Exception {
-        MongoClient mongoClient = new MongoClient(host, port);
-        return new SimpleMongoDbFactory(mongoClient, database);
+	MongoClientURI uri = new MongoClientURI("mongodb://"+userName+":"+password+"@"+host+":"+port+"/"+database);
+	return new SimpleMongoDbFactory(uri);
     }
  
     @Bean
